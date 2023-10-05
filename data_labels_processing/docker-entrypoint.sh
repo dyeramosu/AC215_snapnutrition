0550ea83e9c15211f5d1a09e6ba5f0975da31bf6
chimkens <wschristina@gmail.com>
2023-10-05 16:09:52 -0400

    [dvc-gcs-vm] Adding container to process labels

--- data_labels_processing/docker-entrypoint.sh
+++ data_labels_processing/docker-entrypoint.sh
@@ -7,7 +7,9 @@ echo "Container is running!"
 # Set GCP Project Details
 #gcloud config set project $GCP_PROJECT
 
-pipenv install
+pip install pipenv
+pipenv requirements > requirements.txt
+pip install -r requirements.txt
 git config --global user.name "wschristina"
 git config --global user.email "wschristina@gmail.com"
 gcsfuse snapnutrition_data_bucket snapnutrition_data_bucket/
