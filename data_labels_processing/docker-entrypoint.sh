9dc059de4eac92491b5c8814b2a72174aa844b56
chimkens <wschristina@gmail.com>
2023-10-05 15:40:16 -0400

    [dvc-gcs-vm] Adding container to process labels

--- data_labels_processing/docker-entrypoint.sh
+++ data_labels_processing/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Container is running!!!"
+echo "SANITY"
 
 # Authenticate gcloud using service account
 #gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
