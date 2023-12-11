e15db3d647bbab6d114039df429786d697aee25c
Russell Brown <r.n.brown314@gmail.com>
2023-12-11 18:55:48 -0500

    Update docker-entrypoint.sh
    /run- Add extra node to test autorecovery

--- app/src/deployment/docker-entrypoint.sh
+++ app/src/deployment/docker-entrypoint.sh
@@ -11,5 +11,5 @@ gcloud auth configure-docker gcr.io -q
 
 /bin/bash
 #CMD ["ansible-playbook", "deploy-k8s-cluster.yml", "-i", "inventory.yml", "--extra-vars", "cluster_state=present"]
-gcloud container clusters create snapnutrition-app-cluster --machine-type=n2d-standard-2 --num-nodes=1 --service-account=deployment-russell@csci-115-398800.iam.gserviceaccount.com --zone=us-central1-a
+gcloud container clusters create snapnutrition-app-cluster --machine-type=n2d-standard-2 --num-nodes=2 --service-account=deployment-russell@csci-115-398800.iam.gserviceaccount.com --zone=us-central1-a
 ansible-playbook deploy-k8s-cluster.yml -i inventory.yml --extra-vars cluster_state=present
